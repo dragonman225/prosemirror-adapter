81f09ed2dc86acbb424b777372a23978fa4ce7c3
Alexander Wang <25148955+dragonman225@users.noreply.github.com>
2024-06-19 18:53:04 +0000

    add a button to dynamically enable/disable decoration plugin

--- examples/react/components/Editor.tsx
+++ examples/react/components/Editor.tsx
@@ -4,7 +4,7 @@ import { useNodeViewFactory, usePluginViewFactory, useWidgetViewFactory } from '
 import type { EditorView } from 'prosemirror-view'
 import { DecorationSet } from 'prosemirror-view'
 import type { FC } from 'react'
-import { useCallback, useRef } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 import { Plugin } from 'prosemirror-state'
 import { createEditorView } from '../createEditorView'
@@ -27,11 +27,6 @@ export const Editor: FC = () => {
       if (element.firstChild)
         return
 
-      const getHashWidget = widgetViewFactory({
-        as: 'i',
-        component: Hashes,
-      })
-
       viewRef.current = createEditorView(element, {
         paragraph: nodeViewFactory({
           component: Paragraph,
@@ -47,27 +42,66 @@ export const Editor: FC = () => {
             component: Size,
           }),
         }),
-        new Plugin({
-          props: {
-            decorations(state) {
-              const { $from } = state.selection
-              const node = $from.node()
-              if (node.type.name !== 'heading')
-                return DecorationSet.empty
-
-              const widget = getHashWidget($from.before() + 1, {
-                side: -1,
-                level: node.attrs.level,
-              })
-
-              return DecorationSet.create(state.doc, [widget])
-            },
-          },
-        }),
       ])
     },
-    [nodeViewFactory, pluginViewFactory, widgetViewFactory],
+    [nodeViewFactory, pluginViewFactory],
   )
 
-  return <div className="editor" ref={editorRef} />
+  const getHashWidget = useMemo(() => widgetViewFactory({
+    as: 'i',
+    component: Hashes,
+  }), [widgetViewFactory])
+
+  const decorationPlugin = useMemo(() => new Plugin({
+    props: {
+      decorations(state) {
+        const { $from } = state.selection
+        const node = $from.node()
+        if (node.type.name !== 'heading')
+          return DecorationSet.empty
+
+        const widget = getHashWidget($from.before() + 1, {
+          side: -1,
+          level: node.attrs.level,
+        })
+
+        return DecorationSet.create(state.doc, [widget])
+      },
+    },
+  }), [getHashWidget])
+
+  const [enablePlugin, setEnablePlugin] = useState(true)
+
+  /* Enable or disable the plugin based on a React state. */
+  useEffect(() => {
+    if (!enablePlugin) return
+
+    const view = viewRef.current
+    if (!view) return
+
+    const { state } = view
+    const newState = state.reconfigure({
+      plugins: state.plugins.concat(decorationPlugin),
+    })
+    view.updateState(newState)
+    console.log(newState.plugins.length)
+  
+    return () => {
+      const currentState = view.state
+      const newState = currentState.reconfigure({
+        plugins: currentState.plugins.filter((plugin) => {
+          return plugin !== decorationPlugin
+        }),
+      })
+      view.updateState(newState)
+      console.log(newState.plugins.length)
+    }
+  }, [enablePlugin, decorationPlugin])
+
+  return (
+    <>
+      <div className="editor" ref={editorRef} />
+      <button onClick={() => setEnablePlugin(en => !en)}>{ enablePlugin ? 'disable plugin' : 'enable plugin' }</button>
+    </>
+  )
 }
